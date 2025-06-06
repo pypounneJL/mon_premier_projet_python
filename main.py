ea4e8f84ad5407739894be4bdeef6a5c9ac0f975
jean-luc_stagiaire <jlpoitreau@karibu-gestion.fr>
2025-06-06 11:47:04 +0200

    outils_annexe_age_exact

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from datetime import datetime
+import pandas as pd 
+import json 
+with open ('param.json','r') as file:
+    data=json.load(file)
+table_morta= pd.read_csv("Table unisexe.csv",sep= ';',index_col=0)
+
 
