2429b264314376739bde48576ea34f0ea90ab10e
jean-luc_stagiaire <jlpoitreau@karibu-gestion.fr>
2025-06-06 15:06:34 +0200

    calculs_ages

---
+++ calcul_age.py
@@ -0,0 +1,63 @@
+from decimal import Decimal, ROUND_HALF_UP
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import pandas as pd 
+import calendar
+import json 
+with open ('param.json','r') as file:
+    data=json.load(file)
+
+def arrondi_excel(valeur, nb_decimales):
+    facteur = Decimal('1.' + '0'*nb_decimales)
+    return float(Decimal(str(valeur)).quantize(facteur, rounding=ROUND_HALF_UP))
+
+
+#lire les dates en python
+date_naissance_contractant = datetime.strptime(data['date_naissance_contractant'], "%d/%m/%Y")
+date_effet_rente = datetime.strptime(data['date_effet_rente'], "%d/%m/%Y")
+date_naissance_conjoint = datetime.strptime(data['date_naissance_conjoint'], "%d/%m/%Y")
+#calcul de l'age
+
+def age_exact(date_naissance, date_effet_rente) :
+    """on rentre en argument la date de naissance et la date d'effet de la rente ATTENTION elle doivent eêtre au format date de python """
+
+        # --- Fraction du premier mois ---
+    # Dernier jour du mois de naissance
+    dernier_jour_mois_naissance = calendar.monthrange(date_naissance.year, date_naissance.month)[1]
+
+    fraction_premier_mois = arrondi_excel(
+        (dernier_jour_mois_naissance - date_naissance.day + 1) / dernier_jour_mois_naissance, 3
+    )# --- Nombre de mois pleins entre naissance et mois précédent contrat ---
+    # Date du mois précédent contrat
+    mois_precedent_contrat = date_effet_rente - relativedelta(months=1)
+
+    nb_annees = mois_precedent_contrat.year - date_naissance.year
+    nb_mois = mois_precedent_contrat.month - date_naissance.month
+
+    total_mois = nb_annees * 12 + nb_mois
+    # comme les contrats commencent le 01/m/n on a fini car on a la fractions de mois entre la naissance et la fin du mois de naissance
+    age_exact= arrondi_excel((fraction_premier_mois+total_mois)/12,3)
+    
+    
+    return age_exact
+
+
+
+
+def decimal_age(date_naissance):
+    return arrondi_excel((age_exact(date_naissance , date_effet_rente))-int(age_exact(date_naissance, date_effet_rente)), 3) 
+
+print(decimal_age(date_naissance_contractant))
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ recherche_Lx.py
@@ -0,0 +1,43 @@
+from calcul_age import age_exact, decimal_age
+from decimal import Decimal, ROUND_HALF_UP
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import pandas as pd 
+import calendar
+import json 
+with open ('param.json','r') as file:
+    data=json.load(file)
+table_morta= pd.read_csv("Table unisexe.csv",sep= ';',index_col=0)
+
+#lire les dates en python
+date_naissance_contractant = datetime.strptime(data['date_naissance_contractant'], "%d/%m/%Y")
+date_effet_rente = datetime.strptime(data['date_effet_rente'], "%d/%m/%Y")
+date_naissance_conjoint = datetime.strptime(data['date_naissance_conjoint'], "%d/%m/%Y")
+
+
+age_exact_contractant= age_exact(date_naissance_contractant , date_effet_rente)
+décimal_age_contractant= decimal_age(date_naissance_contractant)
+age_exact_conjoint= age_exact(date_naissance_conjoint, date_effet_rente)
+decimal_age_conjoint= decimal_age(date_naissance_conjoint)
+
+print(age_exact_conjoint)
+#recuperation de la colonne Lx qui est le nombre de personne en vie restante pour un age donné à une année de naissance donnée 
+
+
+
+
+
+def recuperation_Lx(date_naissance):
+    Lx_collonne = []
+    année_naissance=date_naissance.year 
+    for age in range(0, 500) :
+        
+        Lx_collonne.append(table_morta.loc[age , str(année_naissance)])
+    return Lx_collonne
+
+    
+    
+Lx_contractant=recuperation_Lx(date_naissance_contractant)
+print(Lx_contractant)
+Ly_conjoint= recuperation_Lx(date_naissance_conjoint)
+
